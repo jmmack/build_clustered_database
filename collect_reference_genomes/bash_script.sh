974d2a5487928cbedb05d41925f25d8a0ba099e4
Ruth Wong <ruth@cjelli.biochem.fmd.uwo.ca>
2015-09-25 16:24:04 -0400

    added blast output from seed otus

--- collect_reference_genomes/bash_script.sh
+++ collect_reference_genomes/bash_script.sh
@@ -1,1 +1,2 @@
-nohup blastall -i data/OTU_seed_seqs.fa -d hmp_genomes/all_seqs.fa -p blastn -b 10 -v 10 -o output/blast_output.txt &
+blastall -i data/OTU_seed_seqs_less_common_removed.fa -d hmp_genomes/all_seqs.fa -p blastn -b 10 -v 10 -o output/blast_output.txt &
+
