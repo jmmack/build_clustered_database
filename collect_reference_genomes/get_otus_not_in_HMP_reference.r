7255774189639537ddc6c5fda514ae743577a08b
ruthgrace <ruthgracewong@gmail.com>
2015-09-30 13:27:27 -0400

    made otus not in HMP set with new select 8x8 metagenomic smaples'

--- collect_reference_genomes/get_otus_not_in_HMP_reference.r
+++ collect_reference_genomes/get_otus_not_in_HMP_reference.r
@@ -1,3 +1,6 @@
+# add this header to blast.out:
+# qseqid  sseqid  pident  length  mismatch  gapopen qstart  qend  sstart  send  evalue  bitscore  qlen  slen
+
 data <- read.table("output/blast.out",header=TRUE)
 otus.in.hmp <- data[which(data$pident >= 97),]
 otus.not.in.hmp <- unique(data$qseqid[which(!as.character(data$qseqid)%in%as.character(otus.in.hmp$qseqid))])
